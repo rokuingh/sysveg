9de2a1a692effc59b143f3d161e90c308564c1ed
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2018-06-04 09:47:01 -0600

    add ocgis regridding script for hermes data

---
+++ regridding/regrid_hermes_geo_ocgis.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# Ryan O'Kuinghttons
+# May 23, 2018
+
+###############################################################################
+#
+#  Regrid the HERMES emisiones data onto geo grid, run with:
+#
+#  python regrid_hermes_geo_ocgis.py
+#
+###############################################################################
+
+import ESMF
+import ocgis
+
+emi_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/Emisiones_CAT1_2015091_UAB_4.nc"
+emi_file = "/home/ryan/sandbox/sysveg/data/HERMES/Emisiones_CAT1_2015091_UAB_4_grid.nc"
+geo_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/geo_em.d03.nc"
+geo_file = "/home/ryan/sandbox/sysveg/data/HERMES/geo_em.d03-2.nc"
+
+# Regrid using bilinear interpolation (i.e. without corners)
+
+rd_in = ocgis.RequestDataset(uri=emi_file, format_time=False)
+rd_out = ocgis.RequestDataset(uri=geo_file, format_time=False)
+regrid_options = {'regrid_method': ESMF.RegridMethod.BILINEAR}
+
+# import ipdb; ipdb.set_trace()
+
+ops = ocgis.OcgOperations(dataset=rd_in, regrid_destination=rd_out, output_format='nc',
+                          regrid_options=regrid_options, prefix='without_corners')
+ret = ops.execute()
