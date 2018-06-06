887a67434441f369ab26b0d0c0b2c2a489a07d7a
Ryan O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2018-06-06 08:17:31 -0600

    add regridding todos from meeting with isabel

--- regridding/regrid_hermes_geo_ocgis.py
+++ regridding/regrid_hermes_geo_ocgis.py
@@ -8,15 +8,24 @@
 #
 #  python regrid_hermes_geo_ocgis.py
 #
+# 
+# TODO:
+# - change missing values to 0
+# - investigate whether there are more than 2 time steps and if so why
+# - use a conservative RegridMethod
+# - run domain 4 (barcelona)
+# 
+# 
 ###############################################################################
 
 import ESMF
 import ocgis
 
-# emi_file = "/home/ryan/sandbox/sysveg/data/HERMES/Emisiones_CAT1_2015091_UAB_4_grid.nc"
-emi_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/HERMES/Emisiones_CAT1_2015091_UAB_4_grid.nc"
-# geo_file = "/home/ryan/sandbox/sysveg/data/HERMES/geo_em.d03-2.nc"
-geo_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/HERMES/geo_em.d03-2.nc"
+
+emi_file = "/home/ryan/sandbox/sysveg/data/HERMES/Emisiones_CAT1_2015091_UAB_4_grid.nc"
+# emi_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/HERMES/Emisiones_CAT1_2015091_UAB_4_grid.nc"
+geo_file = "/home/ryan/sandbox/sysveg/data/HERMES/geo_em.d03-2.nc"
+# geo_file = "/nfs/pic.es/user/r/rokuingh/sandbox/sysveg/data/HERMES/geo_em.d03-2.nc"
 
 # Regrid using bilinear interpolation (i.e. without corners)
 
